e3e735fe3c572cc12e3941b6a8d00e099580af94
Basharat Ali <basharatalidogar14@gmail.com>
2024-01-16 17:27:54 +0500

    edit readme and add new.md

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -10,7 +10,7 @@ menunavbar.addEventListener("click",(e)=>{
         resnavbar.style="margin-top:65px";
     }else{
         e.target.name="menu";
-        resnavbar.style="margin-top:-210px";
+        resnavbar.style="margin-top:-300px";
     }
 });
 mode.addEventListener("click",(e)=>{
@@ -29,8 +29,8 @@ document.addEventListener("scroll",(e)=>{
     e.preventDefault();
     console.log("Scrolled....");
     if(window.scrollY>10){
-     navbar.style="background-color:#00b243";
+     navbar.style="md:background-color:#00b243";
     }else{
-        navbar.style="background-color:transparent";
+        navbar.style="md:background-color:transparent";
     }
 })
